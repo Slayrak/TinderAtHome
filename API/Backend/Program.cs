6b5c00b1d813361fc030febdac3f6d30afcd74bd
Максим Шимків <322topgamerintheworld228@gmail.com>
2025-11-13 22:25:54 +0200

    Chapter 3 and custom cert

--- API/Backend/Program.cs
+++ API/Backend/Program.cs
@@ -13,8 +13,12 @@ builder.Services.AddDbContext<AppDataContext>(opt =>
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
